27529ebeb5d6c3e36dcca892438a629cc6211b12
zys6024 <zys6024@163.com>
2018-12-03 13:56:07 +0800

    springboot 2.0 新增事件ApplicationReadyEvent切入位置（当前是1.3.2）

--- src/main/java/com/yale/bamboo/BambooApplication.java
+++ src/main/java/com/yale/bamboo/BambooApplication.java
@@ -1,21 +1,28 @@
 package com.yale.bamboo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class BambooApplication {
-	/**
-	 * 重写configure方法，加载启动类
-	 * @param builder
-	 * @return
-	 */
-/*	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(BambooApplication.class);
-	}*/
-
 	public static void main(String[] args) {
 		SpringApplication.run(BambooApplication.class, args);
 	}
+
+	@Bean
+	public DataLoader dataLoader(){
+		return new DataLoader();
+	}
+	@Slf4j
+	static class DataLoader implements CommandLineRunner {
+		@Override
+		public void run(String... strings) throws Exception {
+			log.info("Loading data...");
+		}
+	}
+
 }
---
+++ src/main/java/com/yale/bamboo/EventLog/ApplicationStartedEventListener.java
@@ -0,0 +1,12 @@
+package com.yale.bamboo.EventLog;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
+@Slf4j
+public class ApplicationStartedEventListener implements ApplicationListener<ApplicationStartedEvent> {
+    @Override
+    public void onApplicationEvent(ApplicationStartedEvent applicationStartedEvent) {
+        log.info("......ApplicationStartedEvent......");
+    }
+}
